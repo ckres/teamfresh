46f6b44d30f77e2bb13954f3f563fb9564670945
as40211 <as40211@gmail.com>
2018-03-26 19:39:28 -0400

    Began login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,68 +2,31 @@ import React, { Component } from 'react'
 import { SearchIcon, LocationIcon } from './Icons'
 import DealStore from './stores/DealStore'
 
-import './ViewDeals.css'
+import './Login.css'
 
-export default class ViewDeals extends Component {
+export default class Login extends Component {
   state = {
-    isLoadingDeals: true,
-    deals: null
-  }
 
-  componentDidMount() {
-    DealStore.getDeals().then((deals) => {
-      this.setState({ deals })
-    })
-  }
-  
-  getDealItems() {
-    const { deals } = this.state
-    return deals.map((deal, i) => {
-      const { name, price, price_before, unit, distance, distributor, thumbnail } = deal
-      return (
-        <li
-          key={i}
-          className="deal-item"
-          style={{ backgroundImage: `url(${thumbnail})` }}
-        >
-          <div className="deal-item-container">
-            <div className="deal-distributor-avatar">
-              <img src={`//logo.clearbit.com/${distributor}.com`} alt="distributor" />
-            </div>
-            <div className="deal-description">
-              <div className="distance">
-                <LocationIcon color="#cecece" />
-                {distance} m
-              </div>
-              <div className="deal-name">{name}</div>
-              <div className="deal-price">
-                {`$${price}/${unit}`}&nbsp;<span className="deal-price_before">(was ${price_before})</span>
-              </div>
-            </div>
-          </div>
-        </li>
-      )
-    })
   }
 
   render() {
     const { deals } = this.state
     return (
-      <div className="deals-container">
-        <div className="deals-search">
+      <div className="welcome">
+      <br />
+      <br />
+      <br />
+      <br />
+        <div className="username">
           <input type="text" />
-          <SearchIcon color="gray" />
         </div>
-        {
-          !deals &&
-            "Loading..."
-        }
-        {
-          deals &&
-          <ul className="deals-list">
-            {this.getDealItems()}
-          </ul>
-        }
+          <div className="password">
+          <input type="text" />
+        </div>
+          <button type="button">Login</button>
+          <br />
+          <button type="button">Sign Up</button>
+
       </div>
     )
   }
