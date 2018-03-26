b8740096f6f673042cbf17931299c951d3872348
asuh <asuh@ncsu.edu>
2018-03-26 19:11:34 -0400

    Redirected default page to login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route
 } from 'react-router-dom'
 import { ViewDealsRoute } from './components/routes'
+import { LoginRoute } from './components/routes'
 
 import logo from './assets/logo.png'
 import './App.css'
@@ -21,15 +22,21 @@ class App extends Component {
             exact
             path={'/'}
             render={() => {
-              // default go to homepage
-              return <Redirect to={'/deals'} />
+              //default go to homepage
+              return <Redirect to={'/login'} />
             }}
           />
-          <Route
+            <Route
+            exact
+            path={'/login'}
+            render={() => {
+              return <LoginRoute />
+            }}
+          />
+            <Route
             exact
             path={'/deals'}
             render={() => {
-              // default go to homepage
               return <ViewDealsRoute />
             }}
           />
