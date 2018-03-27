d47e95d1db643043636e04c888fe823999c1d904
ctripoll <ctripoll@yahoo.com>
2018-03-26 21:03:44 -0400

    Created the initial login page with style. Doesn't link to deals yet.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { SearchIcon, LocationIcon } from './Icons'
 import DealStore from './stores/DealStore'
 
 import './Login.css'
+import logoTitle from '../assets/FreshSpire-Brandmark_Logotype-Green.png';
 
 export default class Login extends Component {
   state = {
@@ -12,21 +13,20 @@ export default class Login extends Component {
   render() {
     const { deals } = this.state
     return (
-      <div className="welcome">
-      <br />
-      <br />
-      <br />
-      <br />
-        <div className="username">
-          <input type="text" />
+      <div className="login-container">
+        <img src={logoTitle} alt="FreshSpire" className="img-format"/>
+        <div className="login-credentials">
+          <input type="text" placeholder="Username"/>
         </div>
-          <div className="password">
-          <input type="text" />
+        <div className="login-credentials">
+          <input type="text" placeholder="Password"/>
         </div>
-          <button type="button">Login</button>
-          <br />
-          <button type="button">Sign Up</button>
-
+        <div className="btn-submit">
+          <input type="button" value="Login" />
+        </div>
+        <div className="btn-submit">
+          <input type="button" value="Sign Up"/>
+        </div>
       </div>
     )
   }
