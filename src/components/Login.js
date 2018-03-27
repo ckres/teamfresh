319ed7ebab81edad38596787ca3b5bc8cffe58e9
kayleejean09 <kayleejean09@gmail.com>
2018-03-27 13:56:28 -0400

    Can fade in the login but not fade out

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { SearchIcon, LocationIcon } from './Icons'
 import DealStore from './stores/DealStore'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import './Login.css'
 import logoTitle from '../assets/FreshSpire-Brandmark_Logotype-Green.png';
+
 
 export default class Login extends Component {
   state = {
@@ -14,19 +16,24 @@ export default class Login extends Component {
     const { deals } = this.state
     return (
       <div className="login-container">
-        <img src={logoTitle} alt="FreshSpire" className="img-format"/>
-        <div className="login-credentials">
-          <input type="text" placeholder="Username"/>
-        </div>
-        <div className="login-credentials">
-          <input type="text" placeholder="Password"/>
-        </div>
-        <div className="btn-submit">
-          <input type="button" value="Login" />
-        </div>
-        <div className="btn-submit">
-          <input type="button" value="Sign Up"/>
-        </div>
+      <ReactCSSTransitionGroup
+      transitionName="example" transitionAppear={true}
+      transitionAppearTimeout={3000}>
+	      <img src={logoTitle} alt="FreshSpire" className="img-format"/>
+	      <div className="login-credentials">
+	      	<input type="text" placeholder="Username"/>
+	      </div>
+		  <div className="login-credentials">
+		    <input type="text" placeholder="Password"/>
+		  </div>
+		  <div className="btn-submit">
+		    <input type="button" value="Login" />
+		  </div>
+		  <div className="btn-submit">
+		    <input type="button" value="Sign Up"/>
+		  </div>
+      </ReactCSSTransitionGroup>
+        
       </div>
     )
   }
