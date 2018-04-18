9c1bc7d6e18c0046268b25c78785f95a473be4c0
hlin14 <hlin14@ncsu.edu>
2018-04-17 20:51:26 -0400

    fix error and add margin in the context

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,23 +16,27 @@ export default class Profile extends Component {
           <div className="profile-wrapper">
 				<img src={logoTitle} alt="FreshSpire" className="img-format"/>
 	            <div className="avatar">
-	      	        <img src='http://lorempixel.com/500/500/people' /> 
+	      	        <img src='http://lorempixel.com/500/500/people' alt="profilePhoto" /> 
 	            </div>
-	            <div className="Profile-context">
-					<p>User Name: <userData> Avatar</userData></p>
+				<div className="Profile-context">
+					<p>User Name: Avatar</p>
 				</div>
 				<div className="Profile-context">
-					<p>Email: <userData> Avatar@gmail.com</userData></p>
+					<p>Email: Avatar@gmail.com</p>
 				</div>
 				<div className="Profile-context">
-					<p>Phone: <userData> 201-938-0483</userData></p>
+					<p>Phone: 201-938-0483</p>
 				</div>
 				<div className="Profile-context">
-					<p>Address:  <userData> Wolf Ridge 1655 Raleigh NC 27606</userData></p>
+					<p>Address:  Wolf Ridge 1655 Raleigh NC 27606</p>
 				</div>
 		        <div className="Profile-context">
 			        <p>Previous Order: </p>
-		        </div>		
+			        <ul>
+				        <li>Red Gala Apples</li>
+				        <li>Ladyfinger Bananas</li>
+				    </ul>
+		        </div>		      		
 				<div className="btn-submit">
 					<input type="button" value="Back to Menu"/>
 					<input type="button" value="Change Profile"/>
