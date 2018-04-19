51a5ebe229e83a81c2e6bb18323df07a53a9cc2c
hlin14 <hlin14@ncsu.edu>
2018-04-18 20:03:10 -0400

    modify the CSS and add the top logo

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 
 
 import './Profile.css'
-import logoTitle from '../assets/FreshSpire-Brandmark_Combination-Green.png';
+import logo from '../assets/logo.png'
 
 export default class Profile extends Component {
   state = {
@@ -13,35 +13,38 @@ export default class Profile extends Component {
   render() {
     return (
       <div className="Profile-container">
+          
+      	  <div className="top-logo">
+          		<img src={logo} alt="logo"/>
+          </div>
           <div className="profile-wrapper">
-				<img src={logoTitle} alt="FreshSpire" className="img-format"/>
 	            <div className="avatar">
 	      	        <img src='http://lorempixel.com/500/500/people' alt="profilePhoto" /> 
 	            </div>
-				<div className="Profile-context">
-					<p>User Name: Avatar</p>
-				</div>
-				<div className="Profile-context">
-					<p>Email: Avatar@gmail.com</p>
-				</div>
-				<div className="Profile-context">
-					<p>Phone: 201-938-0483</p>
-				</div>
-				<div className="Profile-context">
-					<p>Address:  Wolf Ridge 1655 Raleigh NC 27606</p>
-				</div>
-		        <div className="Profile-context">
-			        <p>Previous Order: </p>
-			        <ul>
-				        <li>Red Gala Apples</li>
-				        <li>Ladyfinger Bananas</li>
-				    </ul>
-		        </div>		      		
-				<div className="btn-submit">
-					<input type="button" value="Back to Menu"/>
-					<input type="button" value="Change Profile"/>
-				</div>
-			</div>	
+	      </div>
+			<div className="Profile-context">
+				<p>User Name: Avatar</p>
+			</div>
+			<div className="Profile-context">
+				<p>Email: Avatar@gmail.com</p>
+			</div>
+			<div className="Profile-context">
+				<p>Phone: 201-938-0483</p>
+			</div>
+			<div className="Profile-context">
+				<p>Address:  Wolf Ridge 1655 Raleigh NC 27606</p>
+			</div>
+	        <div className="Profile-context">
+		        <p>Previous Order: </p>
+		        <ul>
+			        <li>Red Gala Apples</li>
+			        <li>Ladyfinger Bananas</li>
+			    </ul>
+	        </div>		      		
+			<div className="btn-submit">
+				<input type="button" value="Change Profile"/>
+			</div>
+			
       </div>
     )
   }
