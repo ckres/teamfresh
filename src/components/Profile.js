b479baf68fbc772134018e0fffbe3d262f9d836a
hlin14 <hlin14@ncsu.edu>
2018-04-22 23:00:54 -0400

    add the top down menu

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,22 +6,73 @@ import './Profile.css'
 import logo from '../assets/logo.png'
 
 export default class Profile extends Component {
-  state = {
+state = {
 
-  }
+}
 
-  render() {
-    return (
-      <div className="Profile-container">
-          
-      	  <div className="top-logo">
-          		<img src={logo} alt="logo"/>
-          </div>
-          <div className="profile-wrapper">
+constructor(props){
+    super(props);
+
+    this.state = {
+      show: false
+    }
+    this.doSomething = this.doSomething.bind(this);
+    this.toggleShow = this.toggleShow.bind(this);
+    this.hide = this.hide.bind(this);
+}
+
+doSomething(e){
+	e.preventDefault();
+	console.log(e.target.innerHTML);
+}
+
+toggleShow(){
+	this.setState({show: !this.state.show});
+}
+
+hide(e){
+	if(e && e.relatedTarget){
+		e.relatedTarget.click();
+	}
+	this.setState({show: false});
+}
+
+render() {
+	return (
+	    <div className="Profile-container">
+
+		  	<div className="top-logo" style={{zIndex:999}}>
+		    	<button className="btn-primary" type="button" onClick={this.toggleShow} onBlur={this.hide}>
+		    	<img src={logo} alt="logo" style={{zIndex:999}} />
+		    	</button>
+		    	<div className="dropdown-menu" >
+     			{
+        			this.state.show &&
+     			(
+				    <ul className="dropdown-menu" style={{display: 'block'}}>
+				    <li><div className="btn-submit" style={{zIndex:200}}>
+				    	<input type="button" value="Profile"/>
+				    </div></li>
+				    <li><div className="btn-submit" style={{zIndex:200}}>
+						<input type="button" value="View Deals"/>
+					</div></li>
+				    <li><div className="btn-submit" style={{zIndex:200}}>
+						<input type="button" value="Shopping Cart"/>
+					</div></li>
+					<li><div className="btn-submit" style={{zIndex:200}}>
+						<input type="button" value="Log Out"/>
+					</div></li>
+					<div className="background-circle" style={{zIndex:-1}} ></div>
+				    </ul>
+      			)
+      			}
+        		</div>   
+			</div>
+		    <div className="profile-wrapper">
 	            <div className="avatar">
 	      	        <img src='http://lorempixel.com/500/500/people' alt="profilePhoto" /> 
 	            </div>
-	      </div>
+		    </div>
 			<div className="Profile-context">
 				<p>User Name: Avatar</p>
 			</div>
@@ -42,10 +93,10 @@ export default class Profile extends Component {
 			    </ul>
 	        </div>		      		
 			<div className="btn-submit">
-				<input type="button" value="Change Profile"/>
+				<input type="button" value="Edit Profile"/>
 			</div>
 			
-      </div>
-    )
+	  </div>
+	)
   }
 }
