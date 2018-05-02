2d0425a42e18d26874ee79ab0faaf6a5ab889d77
ctripoll <ctripoll@yahoo.com>
2018-05-01 23:28:33 -0400

    Remastering Order history page for Shraddhas requests

--- src/components/History.js
+++ src/components/History.js
@@ -27,38 +27,39 @@ export default class History extends Component {
 	  getHistoryItems() {
 	    const { history } = this.state
 	    return history.map((history, i) => {
-	      const { name, price, unit, delivery_date, total_cost, amount_purchased, thumbnail, purchase_date } = history
+	      const { order_date, order_num, item1, amount1, cost1, item2, amount2, cost2, item3, amount3, cost3, total } = history
 	      return (
 	        <li
 	          key={i}
-	          className="basket-item"
-	          style={{ backgroundImage: `url(${thumbnail})` }}
+	          className="history-item"
 	        >
-	          <div className="basket-item-container">
-	            <div className="basket-description">
-	            	<div className="information">
-									<table className="table">
-										<tbody>
-											<tr>
-												<td>
-													<div className="basket-name">{name}</div>
-													<div className="basket-price">
-														{`$${price}/${unit}`}&nbsp;
-													</div>
-												</td>
-												<td>	      
-													<div className="purchase-date">Date of Purchase: {purchase_date}</div>
-													<br />
-													<div className="purchased"># Purchased: {amount_purchased}</div>
-													<div className="basket-cost">
-														Total: ${total_cost}
-													</div>
-													<div className="basket-delivery">Delivery Date: {delivery_date}</div>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
+	          <div className="history-item-container">
+	            <div className="history-description">
+						<table className="table-history">
+							<tbody>
+								<th>
+								    <td>
+										<div className="history-name"><h3>Order Placed on {order_date}</h3></div>
+									</td>
+									<td>
+										<div className="history-name"><h3>TOTAL {total}</h3></div>
+									</td>
+									<hr />
+								</th>
+								<tr> 
+									<div className="history-name">• {item1}</div>
+									<div className="history-text">Amount Purchased: {amount1}</div>
+									<div className="history-text">Total Cost for Item: {cost1}</div>
+									<div className="history-name">• {item2}</div>
+									<div className="history-text">Amount Purchased: {amount2}</div>
+									<div className="history-text">Total Cost for Item: {cost2}</div>
+									<div className="history-name">• {item3}</div>
+									<div className="history-text">Amount Purchased: {amount3}</div>
+									<div className="history-text">Total Cost for Item: {cost3}</div>
+									<div className="ordernum">ORDER {order_num}</div>
+								</tr>
+							</tbody>
+						</table>
 	            </div>
 	          </div>
 	        </li>
@@ -69,19 +70,19 @@ export default class History extends Component {
   render() {
 	  const { isLoadingHistory } = this.state
 	    return (
-	      <div className="baskets-container">
+	      <div className="historys-container">
 	        <Menu />
 	        <br/>
 	        <div className="header">
 	        	<label>Order History</label>
         	</div>
 	        {
-						isLoadingHistory ?
-							<LoadingSpinner />
-							:
-							<ul className="baskets-list">
-								{this.getHistoryItems()}
-							</ul>
+				isLoadingHistory ?
+					<LoadingSpinner />
+					:
+					<ul className="historys-list">
+						{this.getHistoryItems()}
+					</ul>
 	        }
 	      </div>
 	    )
